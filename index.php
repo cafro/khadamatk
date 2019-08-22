cc24cb5b7aedd36204e557226dba5bd8b1f44fbc
Danial Keimasi <danialkeimasi@gmail.com>
2019-08-23 01:50:57 +0430

    init commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$router = new AltoRouter();
+$router->setBasePath('');
+
+
+$router->map( 'GET', '/', function() {
+	require __DIR__ . '/views/home.php';
+});
+
+$router->map( 'POST', '/', function() {
+	require __DIR__ . '/views/home.php';
+});
+
+$router->map( 'GET', '/about', function() {
+	require __DIR__ . 'views/about.php';
+});
+
+$router->map( 'GET', '/blog', function() {
+	require __DIR__ . 'views/blog.php';
+});
+
+$router->map( 'GET', '/blog/[i:id]', function( $id ) {
+	require __DIR__ . '/views/post.php';
+});
+
+
+$match = $router->match();
+
+if( is_array($match) && is_callable( $match['target'] ) ) {
+	call_user_func_array( $match['target'], $match['params'] );
+} else {
+    require 'views/404.html';
+    // header("HTTP/1.0 404 Not Found");
+	// header( $_SERVER["SERVER_PROTOCOL"] . ' 404 Not Found');
+}
