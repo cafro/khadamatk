9563d698a308e2e640a18197314aeb2969446ee8
Danial Keimasi <danialkeimasi@gmail.com>
2019-11-08 18:52:55 +0330

    add hidden link

--- views/home.php
+++ views/home.php
@@ -362,6 +362,7 @@
             قالب تک صفحه ای <a href="https://barnamenevisan.org/"> امپریال</a>
             <a hidden href="https://aradmusic.ir/">دانلود آهنگ جدید</a>
             <a href="http://link.qazvin-solar-ups.ir/links.php">تبادل لینک اتوماتیک</a>
+            <a href="http://www.2mihan.com">تبلیغات رایگان</a>
           </div>
         </div>
       </div>
