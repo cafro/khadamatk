368b6928e6f49e04dfde80c9f7593ee7eb25f981
Danial Keimasi <danialkeimasi@gmail.com>
2019-11-08 19:24:16 +0330

    add hidden link

--- index.php
+++ index.php
@@ -2,13 +2,30 @@
 
 require 'vendor/autoload.php';
 $configs = include('config.php');
+$mongoClient = new \MongoClient(); // legacy class!
+
 
 $router = new AltoRouter();
 $router->setBasePath('');
+$router->addMatchTypes([
+	'slug' => '[0-9A-Za-z_-]++'
+  ]);
+
 
 $router->map( 'GET|POST', '/', function() {
 	require __DIR__ . '/views/home.php';
 });
+
+
+$router->map( 'GET', '/blog/[i:id]', function( $id ) {
+	require __DIR__ . '/views/post.php';
+});
+
+
+$router->map( 'GET', '/blog/[i:id]/[slug:postslug]', function( $id, $postslug = NULL ) {
+	require __DIR__ . '/views/post.php';
+});
+
 
 // $router->map( 'GET', '/about', function() {
 // 	require __DIR__ . 'views/about.php';
@@ -16,10 +33,6 @@ $router->map( 'GET|POST', '/', function() {
 
 // $router->map( 'GET', '/blog', function() {
 // 	require __DIR__ . 'views/blog.php';
-// });
-
-// $router->map( 'GET', '/blog/[i:id]', function( $id ) {
-// 	require __DIR__ . '/views/post.php';
 // });
 
 $match = $router->match();
